0fe8ebd65b7ccab9b969662e6c2a3e5a0876eee7
Michael J Peppers <mcgdevfunk824@protonmail.com>
2018-05-11 00:53:34 -0700

    Create bahamas.js

---
+++ assets/js/cities/bahamas.js
@@ -0,0 +1,29 @@
+$.ajax({
+    url : 'https://api.unsplash.com/photos/random?collections=2097369&fit=clip&w=1920&h=1080&client_id=yourunsplashedAPIkeygoeshere',
+    type : 'GET',
+    dataType : 'json',
+    success : function(data) {
+      console.log(data);
+          
+      // Get photo url
+      var imageurl = data.urls.custom;
+      // Get photographer name
+      var photog = data.user.name;
+      // Get link to photographer portfolio
+      var photogurl = data.user.links.html;
+      
+      
+      // Hide the images on load
+      $('.image-container').hide();
+  
+      // Get a random image from Unsplash and fade it in on load
+      $('.bahamas').css({
+        // serves a random image from this collection on Unsplash.com
+        'background-image' : 'url(' + imageurl + ')'
+      }).fadeIn(1500);
+      
+   // Credit photographer in main menu
+   $('.photog-credit').html('Photog // <a href="' + photogurl + '?utm_source=mothersday&utm_medium=referral&utm_campaign=api-credit" target="_blank">' + photog + '</a>');
+}
+
+});
